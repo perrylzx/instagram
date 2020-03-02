84e6e8fdd136512fc0c933b39c05fba883b3a49b
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-02 18:36:38 +0800

    removed unnecessary code and comments

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,20 +1,6 @@
-// import functions from "firebase-functions";
-// import admin from "firebase-admin";
-// admin.initializeApp();
-
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
 
 //TODO(PERRY): read these in detail
 exports.addMessage = functions.https.onRequest(async (req, res) => {
