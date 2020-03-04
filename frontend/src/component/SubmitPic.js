850a708f8d2532b9a0f40fcef3a2aced022bd8c3
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-04 19:03:09 +0800

    modified component to send uploaded image to cloud storage then send the image url to createPost cloud function

--- frontend/src/component/SubmitPic.js
+++ frontend/src/component/SubmitPic.js
@@ -1,17 +1,42 @@
 import React from "react";
+import firebase from "../firebase";
 
-// This sends your photo to a test api that i made to experiment with input file and form sending req
-const SubmitPic = () => (
-  <form action="http://localhost:4000/submit-photo" method="get">
-    <div>
-      <label for="pic">Upload Photo</label>
-    </div>
-    <br />
-    <div>
-      <input type="file" accept="image/*" id="pic"></input>
-    </div>
-    <br />
-  </form>
-);
+// TODO(Perry): Make this component look fancy
+const SubmitPic = () => {
+  const storage = firebase.storage();
+  const storageRefRoot = storage.ref();
+  const imageRef = storageRefRoot.child("images");
+  return (
+    <form>
+      <input
+        type="file"
+        onChange={e => {
+          const file = e.target.files[0];
+          const storageRef = imageRef.child(file.name);
+          storageRef.put(file).then(async snapshot => {
+            const imageUrl = await snapshot.ref.getDownloadURL();
+            console.log(`Uploaded ${file.name}`, "at", imageUrl);
+            fetch(
+              "https://us-central1-kittengram-9e684.cloudfunctions.net/createPost",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ imageUrl })
+              }
+            );
+          });
+        }}
+      ></input>
+
+      <div>
+        <label>Upload Photo</label>
+      </div>
+
+      <br />
+    </form>
+  );
+};
 
 export default SubmitPic;
