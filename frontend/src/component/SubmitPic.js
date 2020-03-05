0b8c7a989e9284894738e2527541fd7d8eed2eb7
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-05 19:14:44 +0800

    imported css file, put components in divs for flexbox and styling

--- frontend/src/component/SubmitPic.js
+++ frontend/src/component/SubmitPic.js
@@ -1,41 +1,49 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import firebase from "../firebase";
+import "./SubmitPic.css";
 
-// TODO(Perry): Make this component look fancy
+// TODO(Perry): Make this component look fancy with bootstrap
 const SubmitPic = () => {
   const storage = firebase.storage();
   const storageRefRoot = storage.ref();
   const imageRef = storageRefRoot.child("images");
   return (
-    <form>
-      <input
-        type="file"
-        onChange={e => {
-          const file = e.target.files[0];
-          const storageRef = imageRef.child(file.name);
-          storageRef.put(file).then(async snapshot => {
-            const imageUrl = await snapshot.ref.getDownloadURL();
-            console.log(`Uploaded ${file.name}`, "at", imageUrl);
-            fetch(
-              "https://us-central1-kittengram-9e684.cloudfunctions.net/createPost",
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ imageUrl })
-              }
-            );
-          });
-        }}
-      ></input>
-
+    <div className="upload-container">
       <div>
-        <label>Upload Photo</label>
+        <h1 className="upload-text">KITTYGRAM</h1>
       </div>
-
       <br />
-    </form>
+      <div>
+        <form>
+          <input
+            className="upload-text"
+            type="file"
+            onChange={e => {
+              const file = e.target.files[0];
+              const storageRef = imageRef.child(file.name);
+              storageRef.put(file).then(async snapshot => {
+                const imageUrl = await snapshot.ref.getDownloadURL();
+                console.log(`Uploaded ${file.name}`, "at", imageUrl);
+                fetch(
+                  "https://us-central1-kittengram-9e684.cloudfunctions.net/createPost",
+                  {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ imageUrl })
+                  }
+                ).then(res => {
+                  console.log(res);
+                });
+              });
+            }}
+          ></input>
+          <br />
+        </form>
+      </div>
+    </div>
   );
 };
 
