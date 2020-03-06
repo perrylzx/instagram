d123c81933226805fb9658304b52fb0aa74f5cf3
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-07 00:10:22 +0800

    imported fetchposts and added them in the app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import FetchPosts from "./component/FetchPosts";
 import SubmitPic from "./component/SubmitPic";
 
 function App() {
   return (
     <div>
       <SubmitPic />
+      <FetchPosts />
     </div>
   );
 }
