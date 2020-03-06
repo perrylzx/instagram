970a0e3f3f7110e65a17e50e066ff4d967eb2b45
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-07 00:15:33 +0800

    created fetchposts component that fetches image urls from fetchPost cloud function and renders it

--- frontend/src/component/fetchPosts.js
+++ frontend/src/component/fetchPosts.js
@@ -1,1 +1,38 @@
-// TODO(Perry): Make component for fetchPosts
+import React from "react";
+import "./FetchPosts.css";
+
+// This component fetches the urls from the fetchPosts cloud functions, then sets it to the state, on component mount
+class FetchPosts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { imageUrl: [] };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://us-central1-kittengram-9e684.cloudfunctions.net/fetchPosts",
+      {
+        method: "GET"
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(urlList => {
+        this.setState({ imageUrl: urlList });
+      });
+  }
+
+  // Currently, this just renders the first two images from the database
+  // TODO(Perry): Render ALL the images from the database
+  render() {
+    return (
+      <div className="posts-container">
+        <img src={this.state.imageUrl[0]} alt="cat"></img>
+        <img src={this.state.imageUrl[1]} alt="cat"></img>
+      </div>
+    );
+  }
+}
+
+export default FetchPosts;
