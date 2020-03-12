5ae71cbe9ea2abf0b617312549426a7842b484d1
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-12 18:01:16 +0800

    changed functional component to class component to handle state and realtime database updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,39 @@ import React from "react";
 import Login from "./component/Login";
 import FetchPosts from "./component/PostList";
 import SubmitPic from "./component/SubmitPic";
+import firebase from "./firebase";
 
-function App() {
-  return (
-    <div>
-      <Login />
-      <SubmitPic />
-      <FetchPosts />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  render() {
+    const homepage = firebase.auth().currentUser ? (
+      <div>
+        <Login />
+        <SubmitPic />
+        <FetchPosts />
+      </div>
+    ) : (
+      <div>
+        <Login />
+        <FetchPosts />
+      </div>
+    );
+    return homepage;
+  }
 }
 
 export default App;
