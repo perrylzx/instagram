c67be04d5758934d9815210aed87c1f8e992067a
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-12 18:06:00 +0800

    changed to class component to handle verification

--- frontend/src/component/SubmitPic.js
+++ frontend/src/component/SubmitPic.js
@@ -4,47 +4,57 @@ import firebase from "../firebase";
 import "./SubmitPic.css";
 
 // TODO(Perry): Make this component look fancy with bootstrap
-const SubmitPic = () => {
-  const storage = firebase.storage();
-  const storageRefRoot = storage.ref();
-  const imageRef = storageRefRoot.child("images");
-  return (
-    <div className="upload-container">
-      <div>
-        <h1 className="upload-text">Instagram</h1>
-      </div>
-      <br />
-      <div>
-        <form>
-          <input
-            className="upload-text"
-            type="file"
-            onChange={e => {
-              const file = e.target.files[0];
-              const storageRef = imageRef.child(file.name);
-              storageRef.put(file).then(async snapshot => {
-                const imageUrl = await snapshot.ref.getDownloadURL();
-                console.log(`Uploaded ${file.name}`, "at", imageUrl);
-                fetch(
-                  "http://localhost:5001/kittengram-9e684/us-central1/createPost",
-                  {
-                    method: "POST",
-                    headers: {
-                      "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({ imageUrl })
-                  }
-                ).then(res => {
-                  console.log(res);
+class SubmitPic extends React.Component {
+  // TODO(Perry): update this component to do user verification
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(async user => {
+      if (user) {
+      }
+    });
+  }
+
+  render() {
+    const storage = firebase.storage();
+    const storageRefRoot = storage.ref();
+    const imageRef = storageRefRoot.child("images");
+    return (
+      <div className="upload-container">
+        <div>
+          <h1 className="upload-text">Instagram</h1>
+        </div>
+        <br />
+        <div>
+          <form>
+            <input
+              className="upload-text"
+              type="file"
+              onChange={e => {
+                const file = e.target.files[0];
+                const storageRef = imageRef.child(file.name);
+                storageRef.put(file).then(async snapshot => {
+                  const imageUrl = await snapshot.ref.getDownloadURL();
+                  console.log(`Uploaded ${file.name}`, "at", imageUrl);
+                  fetch(
+                    "https://us-central1-kittengram-9e684.cloudfunctions.net/createPost",
+                    {
+                      method: "POST",
+                      headers: {
+                        "Content-Type": "application/json"
+                      },
+                      body: JSON.stringify({ imageUrl })
+                    }
+                  ).then(res => {
+                    console.log(res);
+                  });
                 });
-              });
-            }}
-          ></input>
-          <br />
-        </form>
+              }}
+            ></input>
+            <br />
+          </form>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SubmitPic;
