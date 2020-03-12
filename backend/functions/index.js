14051b2799dcd2ac6fffaf13b8ff3a02a693e99d
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-03-12 17:58:09 +0800

    changed createPost function to handle tests, moved fetchPost functionality to PostList.js for realtime updates

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,21 +1,22 @@
 //TODO(Perry): Change syntax to es6
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const cors = require("cors")({ origin: true });
+const cors = require("cors")({});
 admin.initializeApp(functions.config().firebase);
 
 const db = admin.firestore();
 
-// ref to the firestore where we want to store the image urls
-let imageRef = db.collection("users/user/posts");
-
 // Create Cloud Function `createPost` takes the image url from the front end, which we then send to the firestore ref
 exports.createPost = functions.https.onRequest((req, res) => {
   return cors(req, res, async () => {
-    let setTest = await imageRef.add({
+    // ref to the firestore where we want to store the image urls
+    let postsCollectionRef = db.collection("users/user/posts");
+    let postDocRef = await postsCollectionRef.add({
       url: req.body.imageUrl
     });
-    res.send("succesfully ran funcion");
+    const postDocSnapshot = await postDocRef.get();
+    console.log(postDocSnapshot.data());
+    res.send(postDocSnapshot.data().url);
   });
 });
 
@@ -27,21 +28,42 @@ exports.fetchPosts = functions.https.onRequest((req, res) => {
     admin
       .auth()
       .verifyIdToken(idToken)
-      .then(decodedToken => {
-        let uid = decodedToken.uid;
-        console.log(uid);
-      });
-    let allPosts = postsRef
-      .get()
-      .then(snapshot => {
-        const urlList = [];
-        snapshot.forEach(doc => {
-          urlList.push(doc.data().url);
-        });
-        res.send(urlList);
-      })
-      .catch(err => {
-        console.log("Error getting documents", err);
+      .then(user => {
+        if (user) {
+          let allPosts = postsRef
+            .get()
+            .then(snapshot => {
+              const urlList = [];
+              snapshot.forEach(doc => {
+                urlList.push(doc.data().url);
+              });
+              res.send(urlList);
+            })
+            .catch(err => {
+              console.log("Error getting documents", err);
+            });
+        }
       });
   });
 });
+
+// let postsRef = db.collection("users/user/posts");
+// exports.fetchPosts = functions.https.onRequest((req, res) => {
+//   return cors(req, res, () => {
+//     const idToken = req.headers.authorization;
+//     admin
+//       .auth()
+//       .verifyIdToken(idToken)
+//       .then(user => {
+//         if (user) {
+//           let allPosts = postsRef.onSnapshot(querySnapshot => {
+//             const urlList = [];
+//             querySnapshot.forEach(doc => {
+//               urlList.push(doc.data().url);
+//             });
+//             res.send(urlList);
+//           });
+//         }
+//       });
+//   });
+// });
